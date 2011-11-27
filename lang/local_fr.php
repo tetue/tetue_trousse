54491829220df585386c4fda006d1fb0dda6afc4
tetue <romy@tetue.net>
2011-11-27 02:51:08 +0100

    On les réécrit tout le temps

---
+++ lang/local_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// Formulaire de login
+'form_forum_identifiants' => 'Connectez-vous',
+'login_login2' => 'Votre courriel&nbsp;:',
+'login_login' => 'Courriel&nbsp;:',
+'login_autre_identifiant' => 'Modifier',
+'login_rester_identifie' => 'Rester identifi&eacute;e quelques jours',
+
+//
+
+'accueil' => 'Accueil'
+
+);
+
+?>
